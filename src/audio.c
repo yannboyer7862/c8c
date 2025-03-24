26bd0f78d244d00a228bc4b8069609eb8de3ebab
Yann BOYER <yann.boyer742@gmail.com>
2025-03-24 20:55:52 +0100

    add missing audio initialization

--- src/audio.c
+++ src/audio.c
@@ -5,6 +5,15 @@ void audio_init(Audio* audio) {
         printf("[ERROR] Unable to initialize SDL2_mixer !\n");
         exit(EXIT_FAILURE);
     }
+
+    audio->beep_sound = Mix_LoadWAV("beep_sound.wav");
+
+    if (audio->beep_sound == NULL) {
+        printf("[ERROR] Unable to initialize the beep sound\n");
+        exit(EXIT_FAILURE);
+    }
+
+    Mix_VolumeChunk(audio->beep_sound, MIX_MAX_VOLUME / 2);
 }
 void audio_play_beep_sound(Audio* audio) {
     int check = Mix_PlayChannel(-1, audio->beep_sound, 0);
